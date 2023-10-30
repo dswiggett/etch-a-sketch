cb816cd0f638c9e190cb37b54e99cbd0a54c9d90
Dennis Swiggett <dennisswiggett11@gmail.com>
2023-10-30 16:42:58 -0400

    added sketching, erase sketch, clear sketch functionality

--- app.js
+++ app.js
@@ -1,16 +1,58 @@
 let gameBox = document.querySelector('.game-box');
 let divs = [];
-let div;
+let element;
+
+// button variables
+let colorPicker = document.querySelector('#color-picker');
+let colorMode = document.querySelector('#color-mode');
+let rainbowColor = document.querySelector('#rainbow-color');
+let eraser = document.querySelector('#eraser');
+let eraserOn = false;
+let clear = document.querySelector('#clear');
 
 // create 16x16 grid of square divs inside game box
 for (let i = 1; i <= 256; i++) {
-    let element = document.createElement('div');
+    element = document.createElement('div');
     element.style.cssText = 'border: 1px solid black; padding: 15px;';
     element.setAttribute('class', 'grid');
     gameBox.appendChild(element);
+
+    divs.push(element);
 }
 
-// use flexbox  to make the square divs appear as a grid
-// DOING THIS CURRENTLY IN STYLESHEET
+// when hovering over element change background color - figure out how to do it with a click then drag, instead of a hover
+etchSketchColors('black');
 
-// using a hover effect, permanently change the colors of the squares
+// color picker
+
+// color mode
+
+// rainbow mode
+
+// erase sketch
+eraser.addEventListener('click', () => {
+    eraserOn = !eraserOn;
+    if (eraserOn === true) {
+        etchSketchColors('white');
+    } else {
+        etchSketchColors('black');
+    }
+})
+
+// clear sketch
+clear.addEventListener('click', () => {
+    divs.forEach(div => {
+        div.style.backgroundColor = 'white';
+    });
+})
+
+// grid size
+
+// function to hold forEach
+function etchSketchColors(color) {
+    divs.forEach(div => {
+        div.addEventListener('mouseenter', () => {
+            div.style.backgroundColor = color;
+        });
+    });
+}
