cf549d5c7e2ca774d9ab79bcf00bae4391d257b9
Dennis Swiggett <dennisswiggett11@gmail.com>
2023-12-05 15:22:53 -0500

    added rainbow mode functionality

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let element;
 let gridNum = document.querySelector('#grid-num');
 let gridSize = document.querySelector('#grid');
 let size = document.querySelector('#grid').value;
+
 // create 16x16 grid of square divs inside game box - readjust grid 
 gridNum.textContent = `${size} x ${size}`;
 createGrid(size);
@@ -26,24 +27,36 @@ function createGrid(grids) {
 //// FUNCTIONALITY ////
 let sketchOn = false;
 let colorOn = false;
+let rainbowOn = false;
 let colorPicker = document.querySelector('#color-picker');
 let rainbowMode = document.querySelector('#rainbow-mode');
 let eraser = document.querySelector('#eraser');
 let clear = document.querySelector('#clear');
+
 // start etch-a-sketch by clicking on grid
 gameBox.addEventListener('click', () => {
     sketchOn = !sketchOn;
-    forEachSketch(sketchOn, 'black', 'white');
+    forEachSketch(sketchOn, 'black', 'white', undefined);
 });
+
 // color picker
 colorPicker.addEventListener('input', () => {
     let colorPicked = colorPicker.value;
     gameBox.addEventListener('click', () => {
         colorOn = !colorOn;
-        forEachSketch(colorOn, colorPicked, 'white');
+        forEachSketch(colorOn, colorPicked, 'white', undefined);
     });
 });
+
 // rainbow mode
+let rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+rainbowMode.addEventListener('click', () => {
+    gameBox.addEventListener('click', () => {
+        rainbowOn = !rainbowOn;
+        let x = 0;
+        forEachSketch(rainbowOn, rainbowColors, 'white', x);
+    });
+});
 
 // eraser
 
@@ -55,12 +68,21 @@ clear.addEventListener('click', () => {
 });
 
 // function to keep from repetition
-function forEachSketch(bool, colorOn, colorOff) {
+function forEachSketch(bool, colorOn, colorOff, x) {
     if (bool) {
         gameBox.style.cursor = 'pointer';
         divs.forEach(div => {
             div.addEventListener('mouseover', () => {
                 div.style.backgroundColor = colorOn;
+                
+                // if rainbow mode is clicked
+                if (bool === rainbowOn) {
+                    div.style.backgroundColor = colorOn[x];
+                    x++;
+                    if (x === 7) {
+                        x = 0;
+                    }
+                }
             });
         });
     }
